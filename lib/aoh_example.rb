9a7950a9d35ac80f9468767baffee797d811212e
Dakota Willingham <kota@vaga.tk>
2020-02-20 02:26:34 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,6 +15,13 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  [
+    [DON_G[:name], DON_G[:occupation]], 
+    [JOELLE_VD[:name], JOELLE_VD[:occupation]],
+    [PAT_M[:name], PAT_M[:occupation]],
+    [KATE_G[:name], KATE_G[:occupation]],
+    [BRUCE_G[:name], BRUCE_G[:occupation]]
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
